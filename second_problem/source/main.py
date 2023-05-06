90b4d1da9e838e95ccc0283c77588a3f571b3aa7
Karlos Alejandro Alfonso <karlos.bruno99@gmail.com>
2023-05-05 20:24:31 -0400

    full solution implemented

--- second_problem/source/main.py
+++ second_problem/source/main.py
@@ -1,14 +1,37 @@
 import Graph as gr
 
-g = gr.Graph()
-g.add_edge(1,2,w=1)
-g.add_edge(1,3,w=1)
-g.add_edge(2,4,w=1)
-g.add_edge(2,5,w=2)
-g.add_edge(2,6,w=4)
-g.add_edge(3,4,w=1)
-g.add_edge(3,5,w=2)
+def init_nodes_dict(graph, value):
+    dic = {}
+    for u in graph.nodes:
+        dic[u] = {}
+        for v in graph.nodes:
+            dic[u][v] = value
+    return dic
 
-print(gr.floyd_warshall(g))
-
+def sol1(w_edges):
+    g = gr.graph_from_edges(w_edges)
     
+    
+    
+def sol2(w_edges):
+    g = gr.graph_from_edges(w_edges)
+    
+    min_dists = gr.floyd_warshall(g)
+    
+    inEdges = init_nodes_dict(g, 0)
+    sol = init_nodes_dict(g, 0)
+    
+    for edge in g.edges:
+        for node in g.nodes:
+            if min_dists[node][edge[0]] + g[edge[0]][edge[1]]['w'] == min_dists[node][edge[1]]:
+                inEdges[node][edge[1]] += 1
+            if min_dists[node][edge[1]] + g[edge[0]][edge[1]]['w'] == min_dists[node][edge[0]]:
+                inEdges[node][edge[0]] += 1
+
+    for v in g.nodes:
+        for u in g.nodes:
+            for k in g.nodes:
+                if min_dists[v][k] + min_dists[k][u] == min_dists[v][u]:
+                    sol[v][u] += inEdges[v][k]
+    
+    return sol
