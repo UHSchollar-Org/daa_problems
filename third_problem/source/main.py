aefbd72b6fbdd5ad28dd2f0d21c3e1e179682e0b
Kar3lio <karelcamilo@icloud.com>
2023-06-11 01:05:57 -0400

    DPLL solution implemented

--- third_problem/source/main.py
+++ third_problem/source/main.py
@@ -1,6 +1,7 @@
-from SAT_solutions import exhaustive_enumeration
+from SAT_solutions import exhaustive_enumeration, DPLL
 
 n, m = 4, 3
-c = ["1 2 3", "-1 3 4", "2 -3 -4"]
+c = ["1 2 3", "-1 -2 4", "2 -3 -4"]
 
 print(exhaustive_enumeration(n, m, c))
+print(DPLL(n,m,c))
